8178009829a10b4e984e52b9d9ae8515b6a31466
bruce998 <26wz.wang@gmail.com>
2018-05-20 12:39:56 +0800

    wang

--- auto_check_docker.sh
+++ auto_check_docker.sh
@@ -43,5 +43,20 @@ fi
 
 
 
+# -----------------------------------------------------------------
+#500 error restart nginx
+domain="vote.lanrenmb.com/Member/index.php"
+url=$(curl -I -m 10 -o /dev/null -s -w %{http_code} $domain )
+if echo "$url" | grep -q "500"; then
+    docker restart web >> $EMAILMESSAGE
+    DATE=`date '+%Y-%m-%d %H:%M:%S'`
+    echo $domain"-"$url-$DATE > $EMAILMESSAGE
+
+    SUBJECT="docker restart web"
+    EMAIL="271059875@qq.com"
+    mail -s "$SUBJECT" "$EMAIL" < $EMAILMESSAGE
+fi
+
+
 
 exit 0
--- auto_kill_all_process.sh
+++ auto_kill_all_process.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 
 ## Note: will kill the top-most process if the $CPU_LOAD is greater than the $CPU_THRESHOLD.
-echo
-echo checking for run-away process ...
 
 CPU_LOAD=$(uptime | cut -d"," -f4 | cut -d":" -f2 | cut -d" " -f2 | sed -e "s/\.//g")
 CPU_THRESHOLD=400
 
-if [ $CPU_LOAD -gt $CPU_THRESHOLD ] ; then
-
-  EMAILMESSAGE="/tmp/emailmessage.txt"
-  echo "System overloading, possible runaway process."> $EMAILMESSAGE
+while [ true ]
+do
 
   while [ $CPU_LOAD -gt $CPU_THRESHOLD ]
   do
-      
+
       CPU_LOAD=$(uptime | cut -d"," -f4 | cut -d":" -f2 | cut -d" " -f2 | sed -e "s/\.//g")
       PROCESS=$(ps aux r)
       TOPPROCESS=$(ps -eo pid -eo pcpu -eo command | sort -k 2 -r | grep -v PID | head -n 1)
 
       #find httpd.itk process
-      SOURCE="httpd.itk" 
+      SOURCE="php-fpm"
       if echo "$TOPPROCESS" | grep -q "$SOURCE"; then
           # kill -9 $(ps -eo pid | sort -k 1 -r | grep -v PID | head -n 1) #original
           # kill -9 $(ps -eo pcpu | sort -k 1 -r | grep -v %CPU | head -n 1)
-           
+
             for pid in $TOPPROCESS   #以空格分隔    32525  8.0 /usr/sbin/httpd.itk
             do
                 echo $pid
@@ -32,15 +28,11 @@ if [ $CPU_LOAD -gt $CPU_THRESHOLD ] ; then
                 break
             done
 
-          echo system overloading!
           echo Top-most process killed $TOPPROCESS
           echo load average is at $CPU_LOAD
+          echo '--------------------------------'
 
-          # Email text/message
-          echo "Top-most process killed $TOPPROCESS" >>$EMAILMESSAGE
-          echo "Load average was at $CPU_LOAD" >>$EMAILMESSAGE
-
-          sleep 2
+          sleep 1
       else
           echo "no match"
           break
@@ -48,21 +40,8 @@ if [ $CPU_LOAD -gt $CPU_THRESHOLD ] ; then
       fi
   done
 
-
-  # send an email using mail
-  SUBJECT="zhejiang kill process"
-  # Email To ?
-  EMAIL="1330407081@qq.com"
-  echo "Active processes..." >>$EMAILMESSAGE
-  echo "$PROCESS" >>$EMAILMESSAGE
-  #mail -s "$SUBJECT" "$EMAIL" < $EMAILMESSAGE
-
-else
- echo
- echo no run-aways.
- echo load average is at $CPU_LOAD
-
-fi
+  sleep 2
+done
 
 exit 0
 
